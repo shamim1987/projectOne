b31311b40b99d4b81a2f08d96fb504a588cd9dcf
shamim1987 <shamim.bhagat@gmail.com>
2016-10-02 00:38:36 +0800

    started finally

---
+++ quiz/js/main.js
@@ -0,0 +1,36 @@
+function numberOfQuestions() {
+
+}
+//It should return an integer that is the number of questions in a game
+function currentQuestion() {
+
+}
+//It should return an integer that is the zero-based index of the current question in the quiz
+function correctAnswer() {
+
+}
+//It should return an integer that is the zero-based index the correct answer for the currrent question
+function semiCorrectAnswer(){
+
+}
+// If there are more than 1 correct answers, the response contains a correct answer and either no answers OR wrong answers
+function numberOfChoices() {
+
+}
+//It should return an integer that is the number of choices for the current question
+function playTurn(choice) {
+
+}
+//It should take a single integer, which specifies which choice the current player wants to make. It should return a boolean true/false if the answer is correct.
+function isGameOver() {
+
+}
+//It should return a true or false if the quiz is over.
+function whoWon() {
+
+}
+//It should return 0 if the game is not yet over, else it should return either 1 or 2 depending on which player won. It should return 3 if the game is a draw.+
+function  restart() {
+
+}
+//It should restart the game so it can be played again.
